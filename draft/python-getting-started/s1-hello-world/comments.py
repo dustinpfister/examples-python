4c3e7002a8ade4db86908517965e80badc7a0e0f
dustinpfister <dustin.pfister@gmail.com>
2020-12-05 09:56:23 -0500

    commnets example

---
+++ draft/python-getting-started/s1-hello-world/comments.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Real python commnets are like that of bash using the # symbol
+
+# There is no real multi-line comment syntax
+# For Python so one way to do it is to just
+# use the # symbol over and over again
+
+"""
+However another trick is to have a multi-line
+String literal or constant if you prefer
+This is not a real comment in python it is
+very much a string value. However by choosing to
+do nothing with it the string value will
+work like a multi-line comment
+"""
