8b0822afefd58e8a8125430958bcfaa0c4bfc076
dustinpfister <dustin.pfister@gmail.com>
2020-12-14 15:56:45 -0500

    type example for s2-var

---
+++ for-post/python-getting-started/s2-var/type.py
@@ -0,0 +1,14 @@
+a = 5
+print( type(a) ) # <class 'int'>
+
+b = 5.7
+print( type(b) ) # <class 'float'>
+
+c = 'string'
+print( type(c) ) # <class 'str'>
+
+d = ['one', 2, 3, 'four']
+print( type(d) ) # <class 'list'>
+
+e = bool(0)
+print( type(e) ) # <class 'bool'>
