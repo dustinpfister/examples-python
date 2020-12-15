d4c9f63900b20d16d1a7ad29c5f31ec7a4643248
dustinpfister <dustin.pfister@gmail.com>
2020-12-15 12:21:09 -0500

    for range example creates a list

--- for-post/python-built-in-functions/s7-range/for.py
+++ for-post/python-built-in-functions/s7-range/for.py
@@ -1,5 +1,7 @@
 import math
 base=2
+nums=[]
 for e in range(0,10):
-  n = int(math.pow(base, e))
-  print(base, e, n)
+  nums.append(int(math.pow(base, e)))
+print(nums)
+# [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
