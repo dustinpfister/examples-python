89bf649f937c8e013b38b81705db148cbd70fbd9
dustinpfister <dustin.pfister@gmail.com>
2020-12-15 13:16:43 -0500

    player-pos example will loop

--- draft/python-library-minecraft-pi/s1-basics/player-pos.py
+++ draft/python-library-minecraft-pi/s1-basics/player-pos.py
@@ -1,11 +1,23 @@
+import threading
 from mcpi import minecraft
 
 mc = minecraft.Minecraft.create()
 
-pos = mc.player.getPos()
-posText = 'player pos: ( ' + str(pos.x) + ', ' + str(pos.y) + ', ' + str(pos.z) + ' )'
+def printPos():
+  pos = mc.player.getPos()
+  posText = 'player pos: ( ' + str(pos.x) + ', ' + str(pos.y) + ', ' + str(pos.z) + ' )'
+  # print pos to python console
+  print(posText);
+  # print pos to minecraft chat
+  mc.postToChat(posText)
 
-# print pos to python console
-print(posText);
-# print pos to minecraft chat
-mc.postToChat(posText)
+def loop(func, sec):
+  def wrapper():
+    loop(func, sec)
+    func()
+  t = threading.Timer(sec, wrapper)
+  t.start()
+  return t
+
+t=loop(printPos, 3)
+t.cancel();
