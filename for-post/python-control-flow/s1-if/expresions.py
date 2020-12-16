87312ba8604365ce6b593920288ee955cf5ec83c
dustinpfister <dustin.pfister@gmail.com>
2020-12-16 13:40:25 -0500

    expressions example

---
+++ for-post/python-control-flow/s1-if/expresions.py
@@ -0,0 +1,11 @@
+x = 5
+y = 10
+
+a = x < y
+print( a, type(a).__name__ ) # True bool
+
+b = x > y
+print( b, type(b).__name__ ) # False bool
+
+c = x == y
+print( c, type(c).__name__ ) # False bool
