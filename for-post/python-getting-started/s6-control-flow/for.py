548daef3eed82d0f4c9606605cd9796f1a1987ff
dustinpfister <dustin.pfister@gmail.com>
2020-12-18 16:42:05 -0500

    while loop example

--- for-post/python-getting-started/s6-control-flow/for.py
+++ for-post/python-getting-started/s6-control-flow/for.py
@@ -1,4 +1,5 @@
 nums = [1,2,3,4,5]
 for n in nums:
     print(pow(2, n), end='-')
+# 2-4-8-16-32-
    
---
+++ for-post/python-getting-started/s6-control-flow/while.py
@@ -0,0 +1,7 @@
+nums = [1,2,3,4,5]
+i = 0
+while i < len(nums):
+    n=nums[i]
+    print(pow(2, n), end='-')
+    i = i + 1
+# 2-4-8-16-32-
