86e578e07ee64b2c0612337b51574c50dc0df5c3
dustinpfister <dustin.pfister@gmail.com>
2020-12-28 16:03:21 -0500

    better basic example

--- for-post/python-data-types/s1-int/basic-int.py
+++ for-post/python-data-types/s1-int/basic-int.py
@@ -1,9 +1,15 @@
+# can start with an int just like this
 x = 1
+print(x, type(x)) # 1 <class 'int'>
+# adding two int values will result in an int
 x = 5 + 1
 print(x, type(x)) # 6 <class 'int'>
 
-x += 0.5;
-print(x, type(x)) # 6.5 <class 'float'>
+# adding a fraction will result in a switch to float
+x += 2.5;
+print(x, type(x)) # 8.5 <class 'float'>
 
-x += 0.5;
-print(x, type(x)) # 7.0 <class 'float'>
+# the int built in function can be used to create
+# an int value from a float
+x = int(x);
+print(x, type(x)) # 8 <class 'int'>
