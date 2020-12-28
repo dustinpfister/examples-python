c6841cf87f6d0efd6b5bb048a9f2ecc8074f4c72
dustinpfister <dustin.pfister@gmail.com>
2020-12-28 15:56:02 -0500

    test for example

---
+++ for-post/python-data-types/sx-none/test-for.py
@@ -0,0 +1,16 @@
+# here is a function that does not return anything
+def testN(n):
+    if(n >= 10):
+        if(n <= 20):
+            return True
+
+def goWith(n):
+    t=testN(n)
+    if(t == True):
+        print('good');
+    if(t == None):
+        print('out of range')
+
+goWith(5)  # 'out of range'
+goWith(15) # 'good'
+goWith(25) # 'out of range'
