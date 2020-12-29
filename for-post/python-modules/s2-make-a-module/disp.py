0b53f27ba29eda898c07c926f014a41eeac41a36
dustinpfister <dustin.pfister@gmail.com>
2020-12-29 16:09:13 -0500

    better disp example

--- for-post/python-modules/s2-make-a-module/disp.py
+++ for-post/python-modules/s2-make-a-module/disp.py
@@ -1,12 +1,19 @@
-
+# create a basic display object
 def createBasic(x=0, y=0, w=32, h=32):
     disp={'x':x,'y':y,'w':w,'h':h}
     return disp
-
-def createPool(count=5, create=createBasic):
+# create an enemy display object
+def createEnemy(x=0,y=0,w=32,h=32,hpMax=100,attack=1):
+    disp=createBasic(x, y, w, h)
+    disp['hpMax']=hpMax
+    disp['hp']=hpMax
+    disp['attack']=attack
+    return disp
+# create a pool of display objects
+def createPool(count=5, create=createBasic, x=0, y=0):
     i=0
-    pool=[]
+    obj=[]
     while i < count:
-        pool.append(create(0,0,32,32))
+        obj.append(create(x,y,32,32))
         i = i + 1
-    return pool
+    return {'obj': obj}
--- for-post/python-modules/s2-make-a-module/script_disp.py
+++ for-post/python-modules/s2-make-a-module/script_disp.py
@@ -2,7 +2,7 @@
 import disp
 
 x=disp.createBasic(5,7)
-b=disp.createPool(10)
+b=disp.createPool(3, x=50, y=25, create=disp.createEnemy)
 
 print(x) # {'x': 5, 'y': 7, 'w': 32, 'h': 32}
-print(b)
+print(b['obj'])
