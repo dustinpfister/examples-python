5095ace6eed3077900ab8c25a153ddaf63afd023
dustinpfister <dustin.pfister@gmail.com>
2020-12-29 15:51:03 -0500

    disp and script_disp examples

---
+++ for-post/python-modules/s2-make-a-module/disp.py
@@ -0,0 +1,6 @@
+
+def createBasic(x=0, y=0, w=32, h=32):
+    disp={'x':x,'y':y,'w':w,'h':h}
+    return disp
+
+#def createPool(count=5, create=createBasic):
---
+++ for-post/python-modules/s2-make-a-module/script_disp.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import disp
+
+x=disp.createBasic(5,7)
+
+print(x) # {'x': 5, 'y': 7, 'w': 32, 'h': 32}
