5a3565f2fe53a01b6c28eb6f16f1bcef67dff403
dustinpfister <dustin.pfister@gmail.com>
2020-12-29 16:12:56 -0500

    commnets

--- for-post/python-modules/s2-make-a-module/script_disp.py
+++ for-post/python-modules/s2-make-a-module/script_disp.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import disp
 
+# I can create a single display object like this
 x=disp.createBasic(5,7)
-b=disp.createPool(3, x=50, y=25, create=disp.createEnemy)
-
 print(x) # {'x': 5, 'y': 7, 'w': 32, 'h': 32}
-print(b['obj'])
+
+# I can create a pool of display objects like this
+b=disp.createPool(3, x=50, y=25, create=disp.createEnemy)
+obj = b['obj']
+print(len(obj)) # 3
+print(obj[2])   # {'x': 50, 'y': 25, 'w': 32, 'h': 32, 'hpMax': 100, 'hp': 100, 'attack': 1}
