d5d8d04392f5f536d7cbe80a9ff0b77a78cb32be
dustinpfister <dustin.pfister@gmail.com>
2020-12-29 15:54:23 -0500

    pool method for disp

--- for-post/python-modules/s2-make-a-module/disp.py
+++ for-post/python-modules/s2-make-a-module/disp.py
@@ -3,4 +3,10 @@ def createBasic(x=0, y=0, w=32, h=32):
     disp={'x':x,'y':y,'w':w,'h':h}
     return disp
 
-#def createPool(count=5, create=createBasic):
+def createPool(count=5, create=createBasic):
+    i=0
+    pool=[]
+    while i < count:
+        pool.append(create(0,0,32,32))
+        i = i + 1
+    return pool
--- for-post/python-modules/s2-make-a-module/script_disp.py
+++ for-post/python-modules/s2-make-a-module/script_disp.py
@@ -2,5 +2,7 @@
 import disp
 
 x=disp.createBasic(5,7)
+b=disp.createPool(10)
 
 print(x) # {'x': 5, 'y': 7, 'w': 32, 'h': 32}
+print(b)
