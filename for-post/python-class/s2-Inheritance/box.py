6309418828510cf5d063f9e5044248eed60ac004
dustinpfister <dustin.pfister@gmail.com>
2020-12-30 14:10:24 -0500

    better box example

--- for-post/python-class/s2-Inheritance/box.py
+++ for-post/python-class/s2-Inheritance/box.py
@@ -1,10 +1,26 @@
 class Box():
+    x=0
+    y=0
+    w=32
+    h=32
     def __init__(self,x=0,y=0,w=32,h=32):
         self.x=x
         self.y=y
         self.w=w
         self.h=h
 
-a=Box()
+class Ship(Box):
+    def __init__(self, hpMax=100, damage=1):
+        self.hpMax = hpMax
+        self.hp = hpMax
+        self.heading = 0
+        self.damage = damage
 
-print(a.__dict__)
+a=Ship()
+
+print(a.hp) # 100
+
+# no properties are set for x,y,w, and h in the Ship Class
+# however the Ship Class inherits from Box and as such the
+# Class level values of Box are used for these values
+print(a.x, a.y)  # 0 0
