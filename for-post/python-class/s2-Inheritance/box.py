19e837854f9fd8595bc53bb8f775e2ea297749a3
dustinpfister <dustin.pfister@gmail.com>
2020-12-30 14:20:50 -0500

    good box example

--- for-post/python-class/s2-Inheritance/box.py
+++ for-post/python-class/s2-Inheritance/box.py
@@ -1,3 +1,5 @@
+import math
+
 class Box():
     x=0
     y=0
@@ -8,6 +10,12 @@ class Box():
         self.y=y
         self.w=w
         self.h=h
+    def distance(self, box2):
+        if(box2 is None):
+            return 0
+        a=math.pow(self.x - box2.x, 2)
+        b=math.pow(self.y - box2.y, 2)
+        return math.sqrt(a+b) 
 
 class Ship(Box):
     def __init__(self, hpMax=100, damage=1):
@@ -24,3 +32,9 @@ print(a.hp) # 100
 # however the Ship Class inherits from Box and as such the
 # Class level values of Box are used for these values
 print(a.x, a.y)  # 0 0
+
+# on top of Box properties functioning as a default for ship values
+# I can also use Box functions with a ship class instance
+b=Box(25,30)
+d=a.distance(b)
+print(d) # 39.05124837953327
