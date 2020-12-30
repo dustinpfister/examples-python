4df8c68d272fa20a89f62ad72f34e7656fa93c61
dustinpfister <dustin.pfister@gmail.com>
2020-12-30 10:06:25 -0500

    basic example

--- draft/python-class/s1-basic/basic.py
+++ draft/python-class/s1-basic/basic.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-class Disp:
-    w=32 # some props for width and height
-    h=32
-    def __init__(self, x, y):
-        self.x = int(x) # can set position when creating
-        self.y = int(y)
-    def getPos(self):
-        return {'x': self.x, 'y': self.y} # just position
-    def getBasicObj(self):
-        obj=self.getPos() # position
-        obj['w']=self.w   # width and height
-        obj['h']=self.w
-        return obj
+class MyClass:
+    a=40
+    c=None
+    def __init__(self, b):
+        self.b=int(b)
+        self.c=self.a + self.b
+    def out(self):
+        print(self.a, self.b, self.c)
 
-x = Disp(3, 5.7)
-
-print(x.getBasicObj()) # {'x': 3, 'y': 5, 'w': 32, 'h': 32}
+x=MyClass(2)
+x.out()
