320442892b6f83db45e5b1e20f7a1bd519c2749d
dustinpfister <dustin.pfister@gmail.com>
2020-12-30 15:39:32 -0500

    looping folder started

---
+++ draft/python-dictionary/s2-looping/items.py
@@ -0,0 +1,5 @@
+knights = {'site': 'dustinpfister.github.io', 'subject': 'python'}
+for key, value in knights.items():
+    print(key, ' ==> ', value, sep="")
+# site ==> dustinpfister.github.io
+# subject ==> python
