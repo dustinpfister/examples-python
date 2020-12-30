360ec90636f5848513f185d28c964e09d77e9283
dustinpfister <dustin.pfister@gmail.com>
2020-12-30 15:09:12 -0500

    named-keys example looks good

--- for-post/python-class/s3-iterators/named-keys.py
+++ for-post/python-class/s3-iterators/named-keys.py
@@ -1,14 +1,25 @@
-class Reverse:
-    """Iterator for looping over a sequence backwards."""
-    def __init__(self, data):
-        self.data = data
-        self.index = len(data)
-
+class Foo:
+    loopKeys=['a','d']
+    def __init__(self, a=1, b=2, c=3, d=4):
+        self.a=a
+        self.b=b
+        self.c=c
+        self.d=d
+        self.index=0
+        
     def __iter__(self):
+        self.index=0
         return self
-
+    
     def __next__(self):
-        if self.index == 0:
+        if self.index == len(Foo.loopKeys):
             raise StopIteration
-        self.index = self.index - 1
-        return self.data[self.index]
+        prop=Foo.loopKeys[self.index]
+        self.index = self.index + 1
+        return self.__dict__[prop]
+
+x=Foo();
+for prop in x:
+    print(prop)
+# 1
+# 4
