3752338d2db19bf510f8731f6b8f133177c80a2d
dustinpfister <dustin.pfister@gmail.com>
2020-12-31 13:10:57 -0500

    keys-values loop example

---
+++ draft/python-dictionary/s2-looping/keys-values.py
@@ -0,0 +1,12 @@
+d = {'site': 'dustinpfister.github.io', 'subject': 'python'}
+
+## keys method
+for key in d.keys():
+    print(key)
+# site
+# subject
+
+for value in d.values():
+    print(value)
+# dustinpfister.github.io
+# python
