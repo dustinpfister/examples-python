1b7d13a456aca664de1a8798942c70a7172f1306
dustinpfister <dustin.pfister@gmail.com>
2020-12-31 14:51:01 -0500

    better basic example

--- for-post/python-dictionary/s1-basics/list-of-dict-keys-values.py
+++ for-post/python-dictionary/s1-basics/list-of-dict-keys-values.py
@@ -1,7 +1,12 @@
 a={'a': 0, 'b': 1}
 b=list(a)
-c=list(a.values())
+c=a.values()
+d=list(c)
 
 print(type(b).__name__) # list
 print(b) # ['a', 'b']
-print(c) # [0, 1]
+
+print(type(c).__name__) # dict_values
+
+print(type(d).__name__) # list
+print(d) # [0, 1]
