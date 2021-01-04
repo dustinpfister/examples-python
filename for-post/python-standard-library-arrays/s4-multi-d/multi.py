57f0d47317268c5dda28e501bbceaf5a3e7481be
dustinpfister <dustin.pfister@gmail.com>
2021-01-04 17:45:43 -0500

    create multi helper

--- for-post/python-standard-library-arrays/s4-multi-d/multi.py
+++ for-post/python-standard-library-arrays/s4-multi-d/multi.py
@@ -1,10 +1,16 @@
 import array
 
-m = array.array('i')
-w = 3
-h = 3
-i=0
-while(i < w * h):
-    m.append(0)
-    i = i + 1
-print(m)
+def createMulti(w=3, h=3):
+    m = array.array('i')
+    i=0
+    while(i < w * h):
+        m.append(0)
+        i = i + 1
+    return {'m': m, 'w': w, 'h': h}
+
+#def getMultiPos(m, x=0, y=0):
+    
+
+a=createMulti()
+
+print(a)
