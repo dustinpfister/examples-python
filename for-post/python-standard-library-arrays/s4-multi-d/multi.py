ac873c99fd1b4622b1df2ac400e077bc590fa2c4
dustinpfister <dustin.pfister@gmail.com>
2021-01-04 17:53:17 -0500

    multi example looks good

--- for-post/python-standard-library-arrays/s4-multi-d/multi.py
+++ for-post/python-standard-library-arrays/s4-multi-d/multi.py
@@ -1,16 +1,16 @@
 import array
 
 def createMulti(w=3, h=3):
-    m = array.array('i')
+    a = array.array('i')
     i=0
     while(i < w * h):
-        m.append(0)
+        a.append(i)
         i = i + 1
-    return {'m': m, 'w': w, 'h': h}
+    return {'a': a, 'w': w, 'h': h}
 
-#def getMultiPos(m, x=0, y=0):
-    
+def getMultiPos(m, x=0, y=0):
+    a=m['a']
+    return a[int(y * m['w'] + x)];
 
-a=createMulti()
-
-print(a)
+m=createMulti(5, 5)
+print(getMultiPos(m, 2, 1)) # 7
