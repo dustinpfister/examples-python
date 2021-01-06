6b6dcee9e11483915129e1dc3e38e284367531ee
dustinpfister <dustin.pfister@gmail.com>
2021-01-06 15:23:04 -0500

    better uname example

--- for-post/python-standard-library-os/s1-methods/name-uname.py
+++ for-post/python-standard-library-os/s1-methods/name-uname.py
@@ -2,4 +2,12 @@ import os
 
 print(os.name) # posix, nt, or java
 
-print( list(os.uname())[0] ) # Linux
+# the uname method can be used to get a
+# better idea of the kind of system that is being used
+# with things like kernal name, kernal version, and
+# system arch.
+uname=os.uname()
+l=list(uname)
+print( l[0] ) # Linux
+print( l[2] ) # 5.4.79-v7+
+print( l[4] ) # armv7l
