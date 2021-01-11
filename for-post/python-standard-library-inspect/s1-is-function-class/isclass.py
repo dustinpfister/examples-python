fca2a01d75beb07cca48a3d00c914feae111719a
dustinpfister <dustin.pfister@gmail.com>
2021-01-11 15:31:21 -0500

    is method

--- for-post/python-standard-library-inspect/s1-is-function-class/isclass.py
+++ for-post/python-standard-library-inspect/s1-is-function-class/isclass.py
@@ -14,3 +14,8 @@ a=P(2, 4)
 print(a.get_p()) # 16
 print(inspect.isclass(P)) # True
 print(inspect.isclass(a)) # False
+
+# when working with a class I want to use
+# is method over is function
+print(inspect.isfunction(a.get_p)) # false
+print(inspect.ismethod(a.get_p))   # True
