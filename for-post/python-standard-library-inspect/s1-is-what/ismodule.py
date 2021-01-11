a1802ee133d75aa67b8ba46db4194aaedac5f212
dustinpfister <dustin.pfister@gmail.com>
2021-01-11 15:58:20 -0500

    get spurce example

--- for-post/python-standard-library-inspect/s1-is-what/ismodule.py
+++ for-post/python-standard-library-inspect/s1-is-what/ismodule.py
@@ -1,2 +1,6 @@
 import inspect
 print(inspect.ismodule(inspect)); # True
+
+# this can slo be done with the type built in
+# function with an expression like this:
+print( type(inspect).__name__ == 'module' ) # True
