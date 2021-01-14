dae92fd971b91b829e29ce828031ad1cdba96852
dustinpfister <dustin.pfister@gmail.com>
2021-01-14 11:29:01 -0500

    find_timeout example

--- for-post/python-standard-library-subprocess/s2-async-timeout/find_timeout.py
+++ for-post/python-standard-library-subprocess/s2-async-timeout/find_timeout.py
@@ -1,15 +1,21 @@
 import subprocess
 
-def findFromFolder(folder='/home'):
+print('foo');
+def findFromFolder(folder='/home', timeout=0.5):
     r=None
     try:
-        r=subprocess.run(['find', '.',  '*.md'], cwd=folder, timeout=0.5, capture_output=True)
+        r=subprocess.run(['find', '.',  '*.md'], cwd=folder, timeout=timeout, capture_output=True)
     except (subprocess.TimeoutExpired):
-        r=None
+        r='time out'
     return r
 
-a=findFromFolder('/home')
-if(a == None):
-    print('no result')
-else:
+
+a=findFromFolder('/', 3)
+print('bar');
+
+if(type(a).__name__ == 'str'):
+    print('mess:', a)
+elif(type(a).__name__ == 'CompletedProcess'):
    print(a.stdout)
+else:
+   print(a)
