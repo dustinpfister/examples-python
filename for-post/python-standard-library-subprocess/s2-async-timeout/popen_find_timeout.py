11802ddb45bbe657027d642f9bb88a260528a56a
dustinpfister <dustin.pfister@gmail.com>
2021-01-14 11:40:13 -0500

    popen example

---
+++ for-post/python-standard-library-subprocess/s2-async-timeout/popen_find_timeout.py
@@ -0,0 +1,21 @@
+import subprocess
+
+print('foo');
+def findFromFolder(folder='/home'):
+    r=None
+    try:
+        r=subprocess.Popen(['find', '.', '-name', '*.js'], cwd=folder)
+    except (subprocess.TimeoutExpired):
+        r='time out'
+    return r
+
+
+a=findFromFolder('/home/pi/js')
+print('bar');
+
+if(type(a).__name__ == 'str'):
+    print('mess:', a)
+elif(type(a).__name__ == 'CompletedProcess'):
+   print(a.stdout)
+else:
+   print(a)
