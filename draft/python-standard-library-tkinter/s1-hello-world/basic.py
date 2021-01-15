24800049edd8858f3c713a4272e0635ee0abdc0b
dustinpfister <dustin.pfister@gmail.com>
2021-01-15 15:18:08 -0500

    redo of basic example

--- draft/python-standard-library-tkinter/s1-hello-world/basic.py
+++ draft/python-standard-library-tkinter/s1-hello-world/basic.py
@@ -1,7 +1,5 @@
-from tkinter import *
-
-root = Tk() 
-a = Label(root, text ="Hello World") 
+import tkinter
+root = tkinter.Tk()
+a = tkinter.Label(root, text ="Hello World") 
 a.pack() 
-  
 root.mainloop() 
