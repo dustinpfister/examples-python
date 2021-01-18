5d3a37a43044e63df0f67f5da83c31eb42aba2ee
dustinpfister <dustin.pfister@gmail.com>
2021-01-18 16:36:45 -0500

    better split example

--- for-post/python-string/s2-methods/split.py
+++ for-post/python-string/s2-methods/split.py
@@ -3,3 +3,18 @@ l = str.split(',')
 
 print(type(l).__name__) # list
 print(l[3]) # 3
+
+str = '012345'
+# I can not give and empty string as a sep
+# doing so will result in an error
+try:
+    l = str.split('')
+except ValueError:
+    print('ValueError')
+
+# however there are a number of other ways to
+# get that kind of list such as passing the string value
+# to the list built in function
+l = list(str);
+print(type(l).__name__) # list
+print(l[3]) # 3
