eefaec173ad537eb570e102433f444d806e44a35
dustinpfister <dustin.pfister@gmail.com>
2021-01-18 16:14:58 -0500

    cap_low example

---
+++ for-post/python-string/s2-methods/cap_low.py
@@ -0,0 +1,11 @@
+a = 'this is SOME text.'
+print(a) # 'this is SOME text.'
+
+b = a.capitalize();
+print(b) # 'This is some text.'
+
+b = a.upper();
+print(b) # 'THIS IS SOME TEXT.'
+
+b = a.lower();
+print(b) # 'this is some text.'
---
+++ for-post/python-string/s2-methods/capitalize.py
@@ -0,0 +1,4 @@
+a = 'this is some text.'
+b = a.capitalize();
+
+print(b) # 'This is some lower case text.'
