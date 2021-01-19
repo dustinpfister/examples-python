c46c7b873f60e8e32240dc570ca58ba8cde05d2c
dustinpfister <dustin.pfister@gmail.com>
2021-01-19 11:27:55 -0500

    better attribute example

--- for-post/python-error/s2-exceptions/attribute-error.py
+++ for-post/python-error/s2-exceptions/attribute-error.py
@@ -1,7 +1,30 @@
+# a key is not the same thing as an attribute
+# if I try to get an attribute that is not an attribute
+# but a key value then that will result in an AttributeError
 d = {'foo': 42}
 try:
-    a = d.bar
+    a = d.foo
 except AttributeError:
     print('AttributeError')
 # output:
 # AttributeError
+
+# to get a key value one way is to use
+# this kind of syntax. However if the value
+# is not there it will result in a KeyError
+print( d['foo'] )
+# output:
+# foo
+try:
+    print( d['bar'] )
+except KeyError:
+    print('KeyError')
+# output:
+# KeyError
+
+## it might be nest to use the get method
+print( d.get('foo', 0) )
+print( d.get('bar', 0) )
+# output:
+# 42
+# 0
