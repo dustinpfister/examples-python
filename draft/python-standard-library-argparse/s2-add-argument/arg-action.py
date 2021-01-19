a6cc6506593fc0948667a2b8d407fa27430ae9dc
dustinpfister <dustin.pfister@gmail.com>
2021-01-19 12:53:20 -0500

    action example

--- draft/python-standard-library-argparse/s2-add-argument/arg-action.py
+++ draft/python-standard-library-argparse/s2-add-argument/arg-action.py
@@ -6,7 +6,15 @@ parser = argparse.ArgumentParser()
 # the argument. If no default is set then the default value for the
 # argument will be None. The default dist for the argument will be the name
 # of the argument
+
 parser.add_argument('--foo')
+
+# here is anoher argument, with values set to what the defaults are
+parser.add_argument('--bar',
+                    action='store',
+                    default=None,
+                    dest='bar')
 
 args = parser.parse_args()
 print(args.foo);
+print(args.bar);
