607d5eb10db589a527a91f055d31d275b1a3bf76
dustinpfister <dustin.pfister@gmail.com>
2021-01-20 16:31:12 -0500

    comments

--- for-post/python-standard-library-re/s2-re-methods/finditer.py
+++ for-post/python-standard-library-re/s2-re-methods/finditer.py
@@ -1,11 +1,9 @@
 import re
-
 a = 'I would say that 654 is also like 123 in some ways'
-
 m = re.finditer('\d+', a);
-
 for i in m:
     print(i.group(0), end=',')
     print(i.start(0), end=',')
     print(i.end(0))
-
+# 654,17,20
+# 123,34,37
