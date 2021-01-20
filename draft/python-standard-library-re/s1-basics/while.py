b9da070654ba8080e0f1b98a7e40da810475682f
dustinpfister <dustin.pfister@gmail.com>
2021-01-20 14:58:02 -0500

    greate while example

--- draft/python-standard-library-re/s1-basics/while.py
+++ draft/python-standard-library-re/s1-basics/while.py
@@ -2,9 +2,20 @@ def find_string(string, sub_string):
     i=0
     sl = len(string)
     subl = len(sub_string)
+    m=[]
     while i <= sl - subl:
-        print(i)
+        text=string[i: i + subl]
+        if(text == sub_string):
+            m.append({
+                "start": i,
+                "end": i + subl,
+                "text": text
+            })
         i = i + 1
+    return m
         
-find_string('easy as abc', 'abc')
-#find_string('not so easy', 'abc')
+print( find_string('abc is easy as abc', 'abc') )
+# [{'start': 0, 'end': 3, 'text': 'abc'}, {'start': 15, 'end': 18, 'text': 'abc'}]
+
+print( find_string('things are not always so easy', 'abc') )
+# []
