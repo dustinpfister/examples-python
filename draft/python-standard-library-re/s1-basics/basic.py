cb13f9a80b67ec8536abcb9df6d2b072e15e691d
dustinpfister <dustin.pfister@gmail.com>
2021-01-20 14:39:40 -0500

    better example

--- draft/python-standard-library-re/s1-basics/basic.py
+++ draft/python-standard-library-re/s1-basics/basic.py
@@ -1,4 +1,10 @@
 import re
 
 m = re.search('foobar', 'All things are foobar when you think about if for a moment')
+
+# the start and end index of the first group
+print(m.start(0)) # 15
+print(m.end(0)) # 15
+
+# the text of the first group
 print(m.group(0)) # foobar
