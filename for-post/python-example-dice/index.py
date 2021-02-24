b9e5a48746137b3e0ddb4fb1eb5f0cfcb1af2605
dustinpfister <dustin.pfister@gmail.com>
2021-02-24 17:01:38 -0500

    sides option working

--- for-post/python-example-dice/index.py
+++ for-post/python-example-dice/index.py
@@ -15,9 +15,21 @@ parser.add_argument('--count',
                     action='store',
                     default=1,
                     help='Set the count of dice (default: 1)')
+parser.add_argument('--sides',
+                    dest='sides',
+                    action='store',
+                    default='6',
+                    help='sides list (default: [6])')
+parser.add_argument('--default_sides',
+                    dest='default_sides',
+                    action='store',
+                    default=6,
+                    help='default side count (default: 6)')
 # parse the arguments
 args = parser.parse_args()
 
+sides = args.sides.split(',');
+
 # using dice lib
-set=dice.roll_set(count=int(args.count))
+set=dice.roll_set( count = int(args.count),  sides = sides )
 print(set)
--- for-post/python-example-dice/lib/dice.py
+++ for-post/python-example-dice/lib/dice.py
@@ -12,11 +12,11 @@ def roll_die(sides=6):
     return random.randint(1, sides)
 
 # roll a set of dice
-def roll_set(count=2, sides=[6, 6], defaultSides=6):
+def roll_set(count=2, sides=['6', '6'], defaultSides=6):
     set=[]
     i=0
     while i < count:
-        s = safe_list_get(sides, i, defaultSides)
+        s = int(safe_list_get(sides, i, defaultSides))
         set.append(roll_die(s))
         i = i + 1
     return set
