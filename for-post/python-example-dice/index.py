dfe8107079f3c4b4ba90066a38bf9cbbc7097ac0
dustinpfister <dustin.pfister@gmail.com>
2021-02-24 13:39:45 -0500

    using a lib folder to hold the dice module

--- for-post/python-example-dice/index.py
+++ for-post/python-example-dice/index.py
@@ -1,4 +1,9 @@
+import os,sys,inspect
+dir_current = os.getcwd()
+dir_script = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+# insert lib folder
+sys.path.insert(0, dir_script + '/lib')
 import dice
-
+# using dice lib
 set=dice.roll_set(count=4)
 print(set)
