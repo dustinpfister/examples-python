af6618e0f3e9bc7e0f8e94c08048e35e2fff5144
dustinpfister <dustin.pfister@gmail.com>
2021-02-24 16:48:25 -0500

    dice lib can now be used to create custom sets of dice

--- for-post/python-example-dice/lib/dice.py
+++ for-post/python-example-dice/lib/dice.py
@@ -1,12 +1,22 @@
 import random;
 
-def roll_die():
-    return random.randint(1, 6)
+# safe list get method
+def safe_list_get (l, idx, default):
+    try:
+        return l[idx]
+    except IndexError:
+        return default
 
-def roll_set(count=2):
+# roll a single die of given sides
+def roll_die(sides=6):
+    return random.randint(1, sides)
+
+# roll a set of dice
+def roll_set(count=2, sides=[6, 6], defaultSides=6):
     set=[]
     i=0
     while i < count:
-        set.append(roll_die())
-        i = i + 1;
+        s = safe_list_get(sides, i, defaultSides)
+        set.append(roll_die(s))
+        i = i + 1
     return set
---
+++ for-post/python-example-dice/todo.md
@@ -0,0 +1,6 @@
+
+* can set number of sides of a dice
+
+* (done) make use of argparse to parse any command line argumnets
+* (done) add lib folder to paths
+* (done) Have a lib folder where the dice.py file will be placed
