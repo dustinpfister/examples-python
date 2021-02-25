91b3d569c7ca3562f331a049ad37c581dea3f1a4
dustinpfister <dustin.pfister@gmail.com>
2021-02-25 18:11:41 -0500

    get state function

--- for-post/python-standard-library-json/s3-open/open.py
+++ for-post/python-standard-library-json/s3-open/open.py
@@ -1,12 +1,31 @@
 import json
 
-f=open('./state.json', 'w+')
+def getState():
+    try:
+        f=open('./state.json', 'r')
+        j=json.loads(f)
+        f.close()
+        print('json load good');
+        return j
+    except:
+        print('json load fail, started new state');
+        return json.dumps({"count": 0})
 
-if f.read() == '':
-    print('blank')
 
+
+state = getState()
+print(state)
+
+#j=f.read()
+#print(j)
+#if j == '':
+#    print('okay');
+#    startState = json.dumps({"count": 0})
+#    f.write(startState)
+#else:
+#    print(f.read());
 #f.write('hello world\n')
-f.close()
+#f.close()
  
 #f=open('./foo.txt', 'r')
 #print(f.read())
