6c6e5ea62177393aa010c519ae7c79edc7857a15
dustinpfister <dustin.pfister@gmail.com>
2021-02-25 18:20:51 -0500

    open example is working

--- for-post/python-standard-library-json/s3-open/open.py
+++ for-post/python-standard-library-json/s3-open/open.py
@@ -1,19 +1,28 @@
 import json
 
-def getState():
+def get_state():
     try:
-        f=open('./state.json', 'r')
-        j=json.loads(f)
+        f = open('./state.json', 'r')
+        j = json.loads(f.read())
         f.close()
         print('json load good');
         return j
     except:
         print('json load fail, started new state');
-        return json.dumps({"count": 0})
+        return {"c": 0}
 
+def put_state(obj):
+    f = open('./state.json', 'w+')
+    j = json.dumps(obj)
+    f.write( j )
+    f.close()
+    return j
 
-
-state = getState()
+state = get_state()
+c = state['c']
+state['c'] = int(c) + 1
+put_state(state)
+print(  )
 print(state)
 
 #j=f.read()
