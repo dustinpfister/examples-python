a9910ee05d4df524f951f61b6d9778531a58ccbf
dustinpfister <dustin.pfister@gmail.com>
2021-02-26 15:32:52 -0500

    comment

--- for-post/python-standard-library-statistics/s3_std_dev/std_dev.py
+++ for-post/python-standard-library-statistics/s3_std_dev/std_dev.py
@@ -1,6 +1,10 @@
 import statistics as stat
+import math
 
 data1=[100,50,75]
 
 print( stat.stdev(data1) )   # 25
+# it is the sqrt of the sample variance
+print( math.sqrt(stat.variance(data1)) ) # 25
+
 print( stat.pstdev(data1) )  # 20.412414523193153
