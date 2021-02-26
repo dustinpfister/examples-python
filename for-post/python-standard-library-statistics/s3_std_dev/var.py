66813bbb4698b7846eff86af7f313fbbcc8efbbe
dustinpfister <dustin.pfister@gmail.com>
2021-02-26 15:28:49 -0500

    var example

--- for-post/python-standard-library-statistics/s3_std_dev/var.py
+++ for-post/python-standard-library-statistics/s3_std_dev/var.py
@@ -2,5 +2,9 @@ import statistics as stat
 
 data1 = [50, 51, 60, 40, 47]
 data2 = [50, 90, 10, 30, 75]
+
 print( stat.pvariance(data1) ) # 41.839999999999996
 print( stat.pvariance(data2) ) # 844
+
+print( stat.variance(data1) ) # 52.3
+print( stat.variance(data2) ) # 1055
